0acfff1ecf418bf576692feff139f6051bb4bc51
Ravi1725 <ankitraj1725@gmail.com>
2021-05-13 11:25:28 +0530

    assignment1 done

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ol>
+        <li key="location1">Delhi</li>
+        <li key="location2">mumbai</li>
+        <li key="location3">Patna</li>
+        <li key="location4">Goa</li>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
